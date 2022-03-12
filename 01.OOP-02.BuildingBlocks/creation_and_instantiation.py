eecedd813bc66c800cefb152fa4c063620286aa0
vanloc1808 <vanloc1808@gmail.com>
2022-03-12 22:10:36 +0700

    creation & instantiation

---
+++ 01.OOP-02.BuildingBlocks/creation_and_instantiation.py
@@ -0,0 +1,11 @@
+class MyClass(object):
+    var = 9
+
+
+# create first instance of MyClass
+this_obj = MyClass()
+print(this_obj.var)
+
+# another instance of MyClass
+that_obj = MyClass()
+print(that_obj.var)
