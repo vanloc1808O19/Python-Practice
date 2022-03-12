cd6ad3e7a7d573fbc32a18274ee12266e82552af
vanloc1808 <vanloc1808@gmail.com>
2022-03-12 20:41:39 +0700

    lists

---
+++ 01.OOP-01.DataStructures/lists.py
@@ -0,0 +1,17 @@
+#  any empty list
+empty_list = []
+
+# a list of strings
+str_list = ['life', 'is', 'beautiful']
+
+# a list of integers
+int_list = [1, 1, 2, 3, 5, 8, 13]
+
+# mixed items list
+mixed_list = ['This', 9, 'is', 1808, 20.09, 'a', 2022, 'mixed', 1203, 'list']
+
+# to print the list
+print(empty_list)
+print(str_list)
+print(int_list)
+print(mixed_list)
