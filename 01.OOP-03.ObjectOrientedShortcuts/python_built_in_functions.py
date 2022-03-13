c6d1c4846e348cfb6c203a39bb5cd985accb5eef
vanloc1808 <vanloc1808@gmail.com>
2022-03-13 10:28:41 +0700

    Python built in functions

---
+++ 01.OOP-03.ObjectOrientedShortcuts/python_built_in_functions.py
@@ -0,0 +1,27 @@
+# len() function
+print(len(['hello', 9, 45.0, 24, True]))
+
+set1 = {1, 2, 3, 4}
+print(set1.__len__())
+
+# reversed(seq)
+normal_list = [2, 4, 5, 7, 9]
+
+
+class CustomSequence:
+    def __len__(self):
+        return 5
+
+    def __getitem__(self, index):
+        return "x{0}".format(index)
+
+
+class FunkyBack:
+    def __reversed__(self):
+        return 'backwards!'
+
+
+for seq in normal_list, CustomSequence(), FunkyBack():
+    print('\n{}'.format(seq.__class__.__name__), end="")
+    for item in reversed(seq):
+        print(item, end=", ")
